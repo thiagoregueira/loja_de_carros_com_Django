4d1fc9a21a276c9090f5b3891c5cc4995a7c5d29
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-12-27 14:26:58 -0300

    allowed_hosts all

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure--7clqr+ejg4mkytr!^tggudxa8pfe^)6xb9!mp=-^#u@zgkeom
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
